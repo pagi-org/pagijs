9305459a77f98b74a1adf2d087211f9c382de563
Jared Bunting <jared.bunting@peachjean.com>
2014-09-06 15:16:00 -0500

    Adding test data download to build.

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
-gulp.task('test', function () {
+gulp.task('test', ['download-testdata'], function () {
     return gulp.src('test/**/*-test.js')
                .pipe(mocha({reporter: 'nyan'}));
 });
