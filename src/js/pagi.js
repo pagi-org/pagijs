880ae13e424bb557bc1beecf57ee524222fa8e8e
Adam Barnhard <adam.barnhard@digitalreasoning.com>
2016-03-26 16:56:04 -0500

    PAGI-158 exporting new functions

--- src/js/pagi.js
+++ src/js/pagi.js
@@ -1,13 +1,25 @@
 var Graph = require('./graph/graph');
 var GraphParser = require('./graph/parsers/graphParser');
 var GraphSerializer = require('./graph/serializers/graphSerializer');
+var validator = require('./validator');
 
 module.exports.parse = function(readableStream) {
-    return GraphParser.parse(readableStream);
+  return GraphParser.parse(readableStream);
 };
+
 module.exports.serialize = function(graph) {
-    if (!(graph instanceof Graph)) { throw Error("`Pagi.serialize` expects a Graph instance."); }
-    return GraphSerializer.serialize(graph);
+  if (!(graph instanceof Graph)) { throw Error("`Pagi.serialize` expects a Graph instance."); }
+  return GraphSerializer.serialize(graph);
+};
+
+module.exports.validateGraph = function(graph, schema) {
+  if (!(graph instanceof Graph)) { throw Error("`Pagi.validateGraph` expects a Graph instance."); }
+  return validator.validateGraph(graph, schema);
+}
+
+module.exports.validateNode = function(id, graph, schema) {
+  if (!(graph instanceof Graph)) { throw Error("`Pagi.validateGraph` expects a Graph instance."); }
+  return validator.validateNode(id, graph, schema);
 };
 
 module.exports.GraphParser = GraphParser;
