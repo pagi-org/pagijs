406e0df4c1fd60c13cc6844f4980b30da8417f2a
Adam Barnhard <adam.barnhard@digitalreasoning.com>
2016-03-28 11:30:05 -0500

    PAGI-158 added property validation

--- src/js/validator/validators/property.js
+++ src/js/validator/validators/property.js
@@ -10,14 +10,14 @@ function isOutOfRange(value, range) {
   return ( range.minRange <= value ) && ( value <= range.maxRange );
 }
 
-function validateIntProp(propVals, propSpec, nodeId) {
+function validateIntProp(prop, propSpec, nodeId) {
   var errors = [];
 
   function isInteger(n) {
     return ( n === +n ) && ( n === (n|0) );
   }
 
-  if (!isInteger(prop.value)) {
+  if (!isInteger(prop.val)) {
     errors.push(validationError(nodeId, [propSpec.name, 'must be an integer'].join(' ')));
   }
 
@@ -34,10 +34,10 @@ function validateIntProp(propVals, propSpec, nodeId) {
   return errors;
 }
 
-function validateFloatProp(propVals, propSpec, nodeId) {
+function validateFloatProp(prop, propSpec, nodeId) {
   var errors = [];
 
-  if (typeof prop.value !== 'number') {
+  if (typeof prop.val !== 'number') {
     errors.push(validationError(nodeId, [propSpec.name, 'must be a float'].join(' ')));
   }
 
@@ -49,15 +49,40 @@ function validateFloatProp(propVals, propSpec, nodeId) {
       'and',
       propSpec.restrictions.maxRange
     ].join(' ')));
+  }
 
   return errors;
 }
 
-function validateStringProp(propVals, propSpec, nodeId) {
+function validateStringRestrictions(prop, restrictions, nodeId) {
+  var errors = [];
 
+  if (restrictions.items.indexOf(prop.val) === -1) {
+    errors.push(validationError(nodeId, [prop.val, 'is not a valid value for property', prop.key].join(' ')));
+  }
+
+  return errors;
 }
 
-function validateBoolProp(propVals, propSpec, nodeId) {
+function validateStringProp(prop, propSpec, nodeId) {
+  var errors = [];
+
+  function valueIsEnumerated(restrictions) {
+    return !!restrictions.items;
+  }
+
+  if (typeof prop.val !== 'string') {
+    errors.push(validationError(nodeId, [propSpec.name, 'must be a string'].join(' ')));
+  }
+
+  if (valueIsEnumerated(propSpec.restrictions)) {
+    errors.push.apply(errors, validateStringRestrictions(prop, propSpec.restrictions, nodeId));
+  }
+
+  return errors;
+}
+
+function validateBoolProp(prop, propSpec, nodeId) {
   var errors = [];
 
   if (typeof prop.value !== 'boolean') {
@@ -74,26 +99,28 @@ module.exports = function validateProperties(node, nodeSpec) {
   var propSpecMap = nodeSpec.propertySpecMap;
 
   Object.keys(propSpecMap).forEach(function(propName) {
-    var propSpec = propertySpecMap[propName];
-    var propVals = nodePropMap[propSpec.name];
+    var propSpec = propSpecMap[propName];
+    var props = nodePropMap[propSpec.name];
 
-    if (propVals) {
-      switch(propSpec.valueType.name) {
-        case 'INTEGER':
-          errors.push.apply(errors, validateIntProp(propVals, propSpec, nodeId));
-          break;
-        case 'FLOAT':
-          errors.push.apply(errors, validateFloatProp(propVals, propSpec, nodeId));
-          break;
-        case 'STRING':
-          errors.push.apply(errors, validateStringProp(propVals, propSpec, nodeId));
-          break;
-        case 'BOOLEAN':
-          errors.push.apply(errors, validateBoolProp(propVals, propSpec, nodeId));
-          break;
-        default:
-          throw new Error('Schema valueType ' + propSpec.valueType.name + ' is not a valid type.');
-      }
+    if (props) {
+      props.forEach(function(prop) {
+        switch(propSpec.valueType.name) {
+          case 'INTEGER':
+            errors.push.apply(errors, validateIntProp(prop, propSpec, nodeId));
+            break;
+          case 'FLOAT':
+            errors.push.apply(errors, validateFloatProp(prop, propSpec, nodeId));
+            break;
+          case 'STRING':
+            errors.push.apply(errors, validateStringProp(prop, propSpec, nodeId));
+            break;
+          case 'BOOLEAN':
+            errors.push.apply(errors, validateBoolProp(prop, propSpec, nodeId));
+            break;
+          default:
+            throw new Error('Schema valueType ' + propSpec.valueType.name + ' not a valid type.');
+        }
+      });
     } else if (isRequired(propSpec)) {
       errors.push(validationError(nodeId, [propSpec.name, 'is a required property'].join(' ')));
     }
